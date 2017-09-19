d94139d35df2d66b055125c44f290963aea8a735
William Chen <William@Williams-MacBook-Pro.local>
2017-09-19 00:28:29 -0400

    Initial Commit

---
+++ Grand Index/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grand Index
+//
+//  Created by William Chen on 19/09/2017.
+//  Copyright © 2017 William Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
